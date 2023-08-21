76fe010bbdae2ef1054f58e9e5dd105f93d57447
Sahil Khan <96828919+S-Khan786@users.noreply.github.com>
2023-08-21 10:33:54 +0530

    Update Code

--- Factorial_of_large_numbers.cpp
+++ Factorial_of_large_numbers.cpp
@@ -1,3 +1,7 @@
+/*So it is usedful to find upto 10000 digits of a number
+If it is factorial occupy 10000 digits of array*/
+/*int store upto 1e9 means 9 digits only*/
+/*long long int store upto 1e18 means 18 digits only*/
 void multiply(vector<int>& arr, int &digit, int no) {
     int carry = 0;
     
@@ -22,10 +26,13 @@ vector<int> factorial(int N){
     int digit = 1;
     
     for(int i=2; i<=N; i++) {
+        //Multiply 1 * 2 * 3 * 4 * 5 and up to N
         multiply(arr, digit, i);
     }
     
     vector<int> ans;
+    //We store answer in reverse order
+    //Print in that order
     for(int i=(digit - 1); i>=0; i--) {
         ans.push_back(arr[i]);
     }
